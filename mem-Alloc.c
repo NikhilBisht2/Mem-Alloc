b3f00c1844e8dbbdce1fe62a7f35f90629016ef6
NikhilBisht2 <nikhilbisht062@gmail.com>
2025-07-03 15:24:31 +0530

    2

--- mem-Alloc.c
+++ mem-Alloc.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <stddef.h>     
-#include <unistd.h>     
-#include <string.h>     
-#include <pthread.h>    
+#include <pthread.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MAX_ALLOC_SIZE (1024 * 1024 * 1024)
 
 struct block {
-    unsigned is_free; 
-    size_t size;
-    struct block *next;
-};
+  unsigned is_free;
+  size_t size;
+  struct block *next;
+} block_t;
 typedef struct block block_t;
 block_t *head = NULL;
 block_t *tail = NULL;
@@ -21,76 +21,141 @@ pthread_mutex_t global_malloc_lock;
 
 // safe for allocation
 int is_safe_allocation(size_t size) {
-    if (size == 0) return 0;
-    if (size > MAX_ALLOC_SIZE) return 0; 
-    if (SIZE_MAX - size < sizeof(struct block)) return 0;
-    return 1;
+  if (size == 0)
+    return 0;
+  if (size > MAX_ALLOC_SIZE)
+    return 0;
+  if (SIZE_MAX - size < sizeof(struct block))
+    return 0;
+  return 1;
 }
 
 // check for free blocks
 block_t *check_for_free(size_t size) {
-    block_t *curr = head;
-    while (curr) {
-        if(curr->is_free && curr->size >= size) {
-            return curr;
-        }
-        curr = curr->next;
+  block_t *curr = head;
+  while (curr) {
+    if (curr->is_free && curr->size >= size) {
+      return curr;
     }
-    return NULL;
+    curr = curr->next;
+  }
+  return NULL;
 }
 
 // malloc
 void *mem_alloc(size_t size) {
-    if(!is_safe_allocation(size)) {
-        return NULL;
-    }
+  if (!is_safe_allocation(size)) {
+    return NULL;
+  }
 
-    size_t align_size = (size + 15) & ~15;
+  size_t align_size = (size + 15) & ~15;
 
-    pthread_mutex_lock(&global_malloc_lock);
+  pthread_mutex_lock(&global_malloc_lock);
 
-    // check for free block
-    block_t *free_block = check_for_free(align_size);
-    if(free_block) {
-        free_block->is_free = 0;
-        pthread_mutex_unlock(&global_malloc_lock);
-        return (void*)(free_block + 1);
-    }
-
-    // get new block from OS
-    block_t *new_block = sbrk(sizeof(block_t) + align_size);
-    if (new_block == (void*)-1) {
-        pthread_mutex_unlock(&global_malloc_lock);
-        return NULL;
-    }
-
-    if(head == NULL) {
-        head = new_block;
-        tail = new_block;
-    }else {
-        tail->next = new_block;
-        tail = tail->next;
-    }
-
-    new_block->is_free = 0;
-    new_block->size = align_size;
-    new_block->next = NULL;
-
+  // check for free block
+  block_t *free_block = check_for_free(align_size);
+  if (free_block) {
+    free_block->is_free = 0;
     pthread_mutex_unlock(&global_malloc_lock);
-    return (void*)(new_block + 1);
+    return (void *)(free_block + 1);
+  }
+
+  // get new block from OS
+  block_t *new_block = sbrk(sizeof(block_t) + align_size);
+  if (new_block == (void *)-1) {
+    pthread_mutex_unlock(&global_malloc_lock);
+    return NULL;
+  }
+
+  if (head == NULL) {
+    head = new_block;
+    tail = new_block;
+  } else {
+    tail->next = new_block;
+    tail = tail->next;
+  }
+
+  new_block->is_free = 0;
+  new_block->size = align_size;
+  new_block->next = NULL;
+
+  pthread_mutex_unlock(&global_malloc_lock);
+  return (void *)(new_block + 1);
 }
 
-// free
-void mem_free() {
+void mem_free(void *ptr) {
+  if (!ptr)
+    return;
 
+  pthread_mutex_lock(&global_malloc_lock);
+
+  block_t *block = (block_t *)ptr - 1;
+  void *program_break = sbrk(0);
+
+  char *block_end = (char *)block + sizeof(block_t) + block->size;
+
+  // Check if the block is at the end of the heap
+  if (block_end == (char *)program_break) {
+    if (head == tail) {
+      head = tail = NULL;
+    } else {
+      block_t *curr = head;
+      while (curr && curr->next != tail) {
+        curr = curr->next;
+      }
+      if (curr) {
+        curr->next = NULL;
+        tail = curr;
+      }
+    }
+
+    sbrk(0 - (sizeof(block_t) + block->size));
+  } else {
+    block->is_free = 1;
+  }
+
+  pthread_mutex_unlock(&global_malloc_lock);
 }
 
 // realloc
-void *re_alloc(size_t size) {
+void *re_alloc(void *ptr, size_t size) {
+  if (!ptr)
+    return mem_alloc(size);
+  if (size == 0) {
+    mem_free(ptr);
+    return NULL;
+  }
 
+  block_t *old_block = (block_t *)ptr - 1;
+
+  if (old_block->size >= size) {
+    return ptr;
+  }
+
+  void *new_ptr = mem_alloc(size);
+  if (!new_ptr)
+    return NULL;
+
+  memcpy(new_ptr, ptr, old_block->size);
+  mem_free(ptr);
+
+  return new_ptr;
 }
 
 // calloc
-void *cal_alloc(size_t size) {
+void *cal_alloc(size_t num, size_t size) {
+  if (!num || !size)
+    return NULL;
 
+  size_t total = num * size;
+
+  if (num != 0 && total / num != size)
+    return NULL;
+
+  void *ptr = mem_alloc(total);
+  if (!ptr)
+    return NULL;
+
+  memset(ptr, 0, total);
+  return ptr;
 }
